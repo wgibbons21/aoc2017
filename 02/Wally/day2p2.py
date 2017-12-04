8753fb508afd13e8b049afb8ae2f36dff5fe2f98
wgibbons21 <wgibbons@gmail.com>
2017-12-03 21:22:18 -0500

    Solved!

--- 02/Wally/day2p2.py
+++ 02/Wally/day2p2.py
@@ -1,12 +1,20 @@
 import numpy as np
 
 contents = np.loadtxt("day2.txt", delimiter="\t")
+contents.sort()
+
 # print(contents)
 checksum = 0
 
-for i in range(0, contents.__len__()):
-    # print("Min: ", min(contents[i]))
-    # print("Max: ", max(contents[i]), "\n")
-    checksum += max(contents[i]) - min(contents[i])
+for i in range(0, contents.shape[0]):
+    # print("i =", i)
+    for j in range(0, contents.shape[1] - 1):
+        # print("j = ", j)
+        for k in range(j, contents.shape[1] - 1):
+            # print("k =", k)
+            if (contents[i][k + 1] / contents[i][j]).is_integer():
+                # print("contents[", i, "][", k + 1, "] / ", "contents[", i, "][", j, "] = ", contents[i][k + 1] / contents[i][j])
+                # print(contents[i][k + 1], "/ ", contents[i][j], "= ", contents[i][k + 1] / contents[i][j])
+                checksum += contents[i][k + 1] / contents[i][j]
 
-print(int(checksum))
+print(checksum)
