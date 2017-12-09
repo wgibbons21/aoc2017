913fa2267c14231ecc4c93efc847ad034324425f
mwinn53 <ltwinn@hotmail.com>
2017-12-08 22:07:39 -0500

    Misinterpreted instructions. Was punished accordingly.

--- 05/mwinn/day5.py
+++ 05/mwinn/day5.py
@@ -24,12 +24,12 @@
 #
 # (0) 3  0  1  -3  - before we have taken any steps.
 # (1) 3  0  1  -3  - jump with offset 0 (that is, don't jump at all). Fortunately, the instruction is incremented to 1.
-#  2 (3) 0  1  -3  - step forward because of the instruction we just modified. The first instruction
 #                    is incremented again, now to 2.
 #  2  4  0  1 (-3) - jump all the way to the end; leave a 4 behind.
 #  2 (4) 0  1  -2  - go back to where we just were; increment -3 to -2.
 #  2  5  0  1  -2  - jump 4 steps forward, escaping the maze.
 # In this example, the exit is reached in 5 steps.
+#  2 (3) 0  1  -3  - step forward because of the instruction we just modified. The first instruction
 #
 # How many steps does it take to reach the exit?
 #
